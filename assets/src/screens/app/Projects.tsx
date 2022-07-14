ea362a0f21f15321c56cbc48ccc90c40bda4b8a1
Tim Su <tim@tandem.chat>
2022-07-14 00:17:41 -0700

    Tweak out the logo and add empty projects

--- assets/src/screens/app/Projects.tsx
+++ assets/src/screens/app/Projects.tsx
@@ -1,3 +1,5 @@
+import NoProjects from '@/screens/app/NoProjects'
+
 type Props = {
   path: string
 }
@@ -6,6 +8,9 @@ export default (props: Props) => (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
       <h1 className="text-2xl font-semibold text-gray-900">Projects</h1>
     </div>
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8"></div>
+
+    <div className="h-8" />
+
+    <NoProjects />
   </>
 )
