5a2c7841aca4fd9efef85fc770cfefc5770edef3
Tim Su <tim@tandem.chat>
2022-07-23 01:06:35 -0700

    Refactor

---
+++ assets/src/components/modals/DeleteModal.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'preact/hooks'
+
+import Button from '@/components/core/Button'
+import ErrorMessage from '@/components/core/ErrorMessage'
+import Modal from '@/components/modals/Modal'
+import { unwrapError } from '@/utils'
+import { Dialog } from '@headlessui/react'
+import { TrashIcon } from '@heroicons/react/outline'
+
+type Props = {
+  label: string
+  close: () => void
+  performAction: () => Promise<void>
+}
+
+export default ({ label, close, performAction }: Props) => {
+  const [error, setError] = useState<string>()
+
+  const submit = async (e: Event) => {
+    e.preventDefault()
+
+    try {
+      await performAction()
+      close()
+    } catch (e) {
+      setError(unwrapError(e))
+    }
+  }
+
+  const onDeletePress = (e: KeyboardEvent) => {
+    if (e.key == 'Escape') close()
+    else submit(e)
+  }
+
+  return (
+    <Modal open={!!open} close={close}>
+      <form onSubmit={submit}>
+        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+          <TrashIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+        </div>
+        <div className="mt-3 text-center sm:mt-5">
+          <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+            Delete {label}?
+          </Dialog.Title>
+        </div>
+        <div className="mt-5 sm:mt-6 flex justify-between">
+          <Button class="bg-gray-500" onClick={close} onKeyPress={close}>
+            Cancel
+          </Button>
+          <Button class="bg-red-500 hover:bg-red-800" onClick={submit} onKeyPress={onDeletePress}>
+            Confirm Delete
+          </Button>
+        </div>
+
+        <ErrorMessage error={error} />
+      </form>
+    </Modal>
+  )
+}
--- assets/src/components/modals/DeleteTaskModal.tsx
+++ assets/src/components/modals/DeleteTaskModal.tsx
@@ -1,22 +1,9 @@
-import { Fragment } from 'preact'
-import { useState } from 'preact/hooks'
-
-import Button from '@/components/core/Button'
-import ErrorMessage from '@/components/core/ErrorMessage'
-import Input from '@/components/core/Input'
-import Submit from '@/components/core/Submit'
-import Modal from '@/components/modals/Modal'
-import { fileStore } from '@/stores/fileStore'
+import DeleteModal from '@/components/modals/DeleteModal'
 import { modalStore } from '@/stores/modalStore'
-import { projectStore } from '@/stores/projectStore'
 import { taskStore } from '@/stores/taskStore'
-import { toTitleCase, unwrapError } from '@/utils'
-import { Dialog, Transition } from '@headlessui/react'
-import { PlusIcon, TrashIcon } from '@heroicons/react/outline'
 import { useStore } from '@nanostores/preact'
 
 export default () => {
-  const [error, setError] = useState<string>()
   const task = useStore(modalStore.deleteTaskModal)
   const open = !!task
 
@@ -24,39 +11,9 @@ export default () => {
 
   const close = () => modalStore.deleteTaskModal.set(false)
 
-  const submit = async (e: Event) => {
-    e.preventDefault()
-
-    try {
-      await taskStore.deleteTask(task)
-      close()
-    } catch (e) {
-      setError(unwrapError(e))
-    }
+  const submit = async () => {
+    await taskStore.deleteTask(task)
   }
 
-  return (
-    <Modal open={!!open} close={close}>
-      <form onSubmit={submit}>
-        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-          <TrashIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-        </div>
-        <div className="mt-3 text-center sm:mt-5">
-          <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-            Delete {task.title}?
-          </Dialog.Title>
-        </div>
-        <div className="mt-5 sm:mt-6 flex justify-between">
-          <Button class="bg-gray-500" onClick={close}>
-            Cancel
-          </Button>
-          <Button class="bg-red-500 hover:bg-red-800" onClick={submit}>
-            Confirm Delete
-          </Button>
-        </div>
-
-        <ErrorMessage error={error} />
-      </form>
-    </Modal>
-  )
+  return <DeleteModal close={close} performAction={submit} label={task.title} />
 }
