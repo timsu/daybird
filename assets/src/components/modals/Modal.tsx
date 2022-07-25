aaba9b15855f6e3bbbcfb54e82df14814060c3b8
Tim Su <tim@listnote.co>
2022-07-25 16:11:59 -0700

    Get quick search working (within this project)

--- assets/src/components/modals/Modal.tsx
+++ assets/src/components/modals/Modal.tsx
@@ -5,11 +5,38 @@ import { Dialog, Transition } from '@headlessui/react'
 type Props = {
   open: boolean
   close: () => void
+  contentClass?: string
 }
 
-export default ({ open, close, children }: RenderableProps<Props>) => {
-  if (!open) return null
+export default ({ children, ...rest }: RenderableProps<Props>) => {
+  if (!rest.open) return null
 
+  return (
+    <ModalBase {...rest}>
+      <Dialog.Panel
+        className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left
+                  overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6"
+      >
+        {children}
+      </Dialog.Panel>
+    </ModalBase>
+  )
+}
+
+export const ModalWithoutPadding = ({ children, ...rest }: RenderableProps<Props>) => {
+  if (!rest.open) return null
+
+  return (
+    <ModalBase {...rest}>
+      <Dialog.Panel className="overflow-hidden rounded-lg bg-white shadow-md w-full max-w-lg">
+        {children}
+      </Dialog.Panel>
+    </ModalBase>
+  )
+}
+
+// modal without any content
+export const ModalBase = ({ open, close, contentClass, children }: RenderableProps<Props>) => {
   return (
     <Transition.Root show={!!open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={close}>
@@ -26,7 +53,12 @@ export default ({ open, close, children }: RenderableProps<Props>) => {
         </Transition.Child>
 
         <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+          <div
+            className={
+              contentClass ||
+              'flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0'
+            }
+          >
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -36,12 +68,7 @@ export default ({ open, close, children }: RenderableProps<Props>) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel
-                className="relative bg-white rounded-lg px-4 pt-5 pb-4 text-left
-                  overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6"
-              >
-                {children}
-              </Dialog.Panel>
+              {children}
             </Transition.Child>
           </div>
         </div>
--- assets/src/components/modals/QuickFindModal.tsx
+++ assets/src/components/modals/QuickFindModal.tsx
@@ -1,13 +1,26 @@
+import { Fragment } from 'preact'
+import { route } from 'preact-router'
 import { useEffect, useState } from 'preact/hooks'
+import { stringSimilarity } from 'string-similarity-js'
 
-import Input from '@/components/core/Input'
-import Modal from '@/components/modals/Modal'
+import { ModalWithoutPadding } from '@/components/modals/Modal'
+import { paths } from '@/config'
+import { DOC_EXT, fileStore } from '@/stores/fileStore'
 import { modalStore } from '@/stores/modalStore'
+import { projectStore } from '@/stores/projectStore'
+import { classNames } from '@/utils'
 import { isMac } from '@/utils/os'
+import { SearchIcon } from '@heroicons/react/outline'
 import { useStore } from '@nanostores/preact'
 
+type SearchResult = {
+  name: string
+  project: string
+  href: string
+  score: number
+}
+
 export default () => {
-  const [name, setName] = useState<string>()
   const open = useStore(modalStore.quickFindModal)
 
   useEffect(() => {
@@ -22,16 +35,122 @@ export default () => {
 
   if (!open) return null
 
+  const close = () => {
+    modalStore.quickFindModal.set(false)
+  }
+
   return (
-    <Modal open={!!open} close={close}>
-      <Input
-        type="text"
-        label={`Jump to a file`}
-        id="name"
-        value={name}
-        autoComplete="off"
-        onChange={(e) => setName((e.target as HTMLInputElement).value)}
-      />
-    </Modal>
+    <ModalWithoutPadding
+      open={open}
+      close={close}
+      contentClass="flex items-start mt-5 sm:mt-20 justify-center min-h-full"
+    >
+      <QuickSearchBody close={close} />
+    </ModalWithoutPadding>
+  )
+}
+
+function QuickSearchBody({ close }: { close: () => void }) {
+  const [searchText, setSearchText] = useState<string>('')
+  const [results, setResults] = useState<SearchResult[] | undefined>()
+  const [selected, setSelected] = useState<number | undefined>()
+
+  useEffect(() => {
+    if (!searchText) {
+      setResults(undefined)
+      return
+    }
+
+    const substringLength = searchText.length < 4 ? searchText.length : undefined
+
+    const files = fileStore.files.get()
+    const project = projectStore.currentProject.get()
+    const results: SearchResult[] = files
+      .filter((f) => f.type == 'doc')
+      .map((f) => ({
+        name: f.name,
+        project: project!.name,
+        href: `${paths.DOC}/${project!.id}/${f.path}`,
+        score: stringSimilarity(searchText, f.name, substringLength),
+      }))
+      .filter((s) => s.score > 0)
+      .sort((a, b) => b.score - a.score)
+      .slice(0, 10)
+
+    setResults(results)
+    setSelected(undefined)
+  }, [searchText])
+
+  useEffect(() => {
+    if (!results?.length) return
+
+    const keyListener = (e: KeyboardEvent) => {
+      console.log(e.key)
+      if (e.key == 'ArrowDown') {
+        e.preventDefault()
+        setSelected((prev) => {
+          const newValue = (prev ?? -1) + 1
+          return Math.min(newValue, results.length - 1)
+        })
+      } else if (e.key == 'ArrowUp') {
+        e.preventDefault()
+        setSelected((prev) => {
+          const newValue = (prev ?? results.length) - 1
+          return Math.max(newValue, 0)
+        })
+      } else if (e.key == 'Enter') {
+        e.preventDefault()
+        setSelected((val) => {
+          const item = results[val ?? 0]
+          if (item) {
+            route(item.href)
+            close()
+          }
+          return val
+        })
+      }
+    }
+    document.addEventListener('keydown', keyListener)
+    return () => document.removeEventListener('keydown', keyListener)
+  }, [results])
+
+  return (
+    <>
+      <div class="relative shadow-sm">
+        <input
+          class="block w-full appearance-none bg-transparent py-4 pl-4 pr-12 text-base
+            text-slate-900 placeholder:text-slate-600 without-ring sm:text-sm sm:leading-6"
+          placeholder="Jump to file..."
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText((e.target as HTMLInputElement).value)}
+        />
+        <SearchIcon class="absolute top-4 right-4 h-6 w-6 text-slate-400" />
+      </div>
+      {results === undefined ? (
+        <></>
+      ) : results.length === 0 ? (
+        <div class="p-8 mx-auto text-sm text-gray-500 uppercase font-semibold">No results</div>
+      ) : (
+        <ul class="max-h-[18.375rem] divide-y divide-slate-200 overflow-y-auto rounded-b-lg border-t border-slate-200 text-sm leading-6">
+          {results.map((r, idx) => (
+            <a href={r.href} key={r.href}>
+              <li
+                class={classNames(
+                  'flex items-center justify-between p-4',
+                  selected == idx ? 'bg-blue-300' : ''
+                )}
+                ref={(elem) =>
+                  selected == idx && elem?.scrollIntoView({ block: 'nearest', inline: 'nearest' })
+                }
+              >
+                <span class="whitespace-nowrap font-semibold text-slate-900">{r.name}</span>
+                <span class="ml-4 text-right text-xs text-slate-600">{r.project}</span>
+              </li>
+            </a>
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
