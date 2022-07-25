62562961d09032521585b09492f9aebe791683e2
Tim Su <tim@tandem.chat>
2022-07-24 20:41:36 -0700

    Small polish

--- assets/src/screens/app/App.tsx
+++ assets/src/screens/app/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'preact/hooks'
+
 import Button from '@/components/core/Button'
 import Loader from '@/components/core/Loader'
 import AppLayout from '@/components/layout/AppLayout'
@@ -6,10 +8,15 @@ import AppRouter from '@/screens/app/AppRouter'
 import { authStore } from '@/stores/authStore'
 import { useStore } from '@nanostores/preact'
 
+const hotModuleReloadId = Math.random()
+
 export default () => {
+  useState(hotModuleReloadId)
   const user = useStore(authStore.loggedInUser)
 
-  if (user === null) location.href = paths.SIGNIN
+  useEffect(() => {
+    if (user === null) location.href = paths.SIGNIN
+  }, [user])
 
   if (!user)
     return (
