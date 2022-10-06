1ba47890b8b3994093c0cfec55fba02e9327a014
Tim Su <tim@listnote.co>
2022-10-06 13:42:58 -0700

    date prefix in new file modal

--- assets/src/components/core/Input.tsx
+++ assets/src/components/core/Input.tsx
@@ -1,9 +1,12 @@
+import { Ref } from 'preact'
+
 type Props = {
   label: string
+  forwardRef: Ref<HTMLInputElement>
 } & JSX.HTMLAttributes<HTMLInputElement>
 
 export default (props: Props) => {
-  const { label, ...rest } = props
+  const { label, forwardRef, ...rest } = props
   return (
     <div className="mb-2">
       {label && (
@@ -13,6 +16,7 @@ export default (props: Props) => {
       )}
       <div className="mt-1">
         <input
+          ref={forwardRef}
           {...rest}
           className="appearance-none block w-full px-3 py-2 border border-gray-300
                       rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500
