49a64362754cd6208be6ea8cae696ed66743af0c
Tim Su <tim@listnote.co>
2022-10-14 00:22:34 -0700

    Change styling of features page

--- assets/src/screens/landing/Features.tsx
+++ assets/src/screens/landing/Features.tsx
@@ -44,10 +44,10 @@ const features = [
 
 export default function () {
   return (
-    <div className="bg-white py-12">
+    <div className="bg-white py-12 px-6">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
-          <h2 className="text-lg font-semibold text-lavender-700">FEATURES</h2>
+          <h2 className="text-lg font-semibold text-lavender-700">Why Daybird?</h2>
           <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
             A better way to plan your day
           </p>
@@ -57,22 +57,26 @@ export default function () {
           </p>
         </div>
 
-        <div className="mt-10">
-          <dl className="space-y-10 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10 md:space-y-0">
+        <div className="mt-10 text-center">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {features.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
-                  <div className="absolute flex h-12 w-12 items-center justify-center rounded-md bg-lavender-500 text-white">
-                    <feature.icon className="h-6 w-6" aria-hidden="true" />
+              <div key={feature.name} className="pt-6">
+                <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center rounded-md bg-lavender-500 p-3 shadow-lg">
+                        <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </span>
+                    </div>
+                    <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
+                      {feature.name}
+                    </h3>
+                    <p className="mt-5 text-base text-gray-500">{feature.description}</p>
                   </div>
-                  <p className="ml-16 text-lg font-medium leading-6 text-gray-900">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
+                </div>
               </div>
             ))}
-          </dl>
+          </div>
         </div>
       </div>
     </div>
