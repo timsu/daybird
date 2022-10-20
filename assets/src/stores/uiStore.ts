8b62d9ef2588cbd3c657906912c6b3b648638d75
Tim Su <tim@daybird.app>
2022-10-20 14:15:44 -0700

    Bring back resume from sleep

--- assets/src/stores/uiStore.ts
+++ assets/src/stores/uiStore.ts
@@ -3,8 +3,13 @@ import { RouterOnChangeArgs } from 'preact-router'
 
 import { config } from '@/config'
 import { File, User } from '@/models'
+import { docStore } from '@/stores/docStore'
+import { fileStore } from '@/stores/fileStore'
+import { projectStore } from '@/stores/projectStore'
 import { topicStore } from '@/stores/topicStore'
+import { logger } from '@/utils'
 
+const SLEEP_CHECK_INTERVAL = 30_000
 const LS_RECENT_FILES = 'rf'
 
 class UIStore {
@@ -33,6 +38,45 @@ class UIStore {
 
     const recentFiles = localStorage.getItem(LS_RECENT_FILES)
     if (recentFiles) this.recentFiles = JSON.parse(recentFiles)
+
+    this.checkForSleep()
+  }
+
+  checkForSleep = () => {
+    let lastCheck = Date.now()
+    let needsRefresh = false
+
+    setInterval(() => {
+      if (Date.now() - lastCheck > 5 * SLEEP_CHECK_INTERVAL) {
+        logger.info(
+          'resume from sleep detected',
+          Date.now() - lastCheck,
+          document.visibilityState,
+          navigator.onLine ? 'online' : 'offline'
+        )
+        if (document.visibilityState == 'visible' && navigator.onLine) this.resumeFromSleep()
+        else needsRefresh = true
+      }
+      lastCheck = Date.now()
+    }, SLEEP_CHECK_INTERVAL)
+
+    document.addEventListener('visibilitychange', () => {
+      if (needsRefresh && navigator.onLine) {
+        this.resumeFromSleep()
+        needsRefresh = false
+      }
+    })
+  }
+
+  resumeFromSleep = () => {
+    const projects = projectStore.projects.get()
+    projects.forEach((p) => {
+      fileStore.loadFiles(p)
+    })
+    const currentDoc = docStore.id.get()
+    if (currentDoc) {
+      docStore.loadDoc(projectStore.currentProject.get()!, currentDoc)
+    }
   }
 
   addRecentNote = (id: string, projectId: string, title: string) => {
