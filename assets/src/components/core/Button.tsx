53aa54aee92d0b0368ced953ff4315723332a164
Tim Su <tim@daybird.app>
2022-10-20 16:54:42 -0700

    Add tailwind merge

--- assets/src/components/core/Button.tsx
+++ assets/src/components/core/Button.tsx
@@ -1,4 +1,5 @@
 import { JSX, RenderableProps } from 'preact'
+import { twMerge } from 'tailwind-merge'
 
 import { classNames } from '@/utils'
 
@@ -7,12 +8,14 @@ type Props = JSX.HTMLAttributes
 export default ({ children, class: cls, className, ...rest }: RenderableProps<Props>) => (
   <button
     type="button"
-    class={classNames(
-      'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm',
-      'font-medium rounded-md text-white bg-lavender-600 hover:bg-lavender-800 focus:outline-none',
-      'focus:ring-2 focus:ring-offset-2 focus:ring-blue-500',
-      cls!,
-      className!
+    class={twMerge(
+      classNames(
+        'inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm',
+        'font-medium rounded-md text-white bg-lavender-600 hover:bg-lavender-800 focus:outline-none',
+        'focus:ring-2 focus:ring-offset-2 focus:ring-blue-500',
+        cls!,
+        className!
+      )
     )}
     {...rest}
   >
