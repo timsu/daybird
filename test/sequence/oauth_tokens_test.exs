71a9289e1a36f2b8404097e1ec5889bb814ab4cb
Tim Su <tim@daybird.app>
2022-10-21 00:34:44 -0700

    Fix tests

--- test/sequence/oauth_tokens_test.exs
+++ test/sequence/oauth_tokens_test.exs
@@ -21,11 +21,11 @@ defmodule Sequence.OAuthTokensTest do
     end
 
     test "create_oauth_token/1 with valid data creates a oauth_token" do
-      valid_attrs = %{access: "some access", user_id: 1, expires_at: ~N[2022-10-19 23:40:00], meta: %{}, name: "some name", refresh: "some refresh"}
+      valid_attrs = %{access: "some access", user_id: 1, expires_at: ~U[2022-10-19 23:40:00Z], meta: %{}, name: "some name", refresh: "some refresh"}
 
       assert {:ok, %OAuthToken{} = oauth_token} = OAuthTokens.create_oauth_token(valid_attrs)
       assert oauth_token.access == "some access"
-      assert oauth_token.expires_at == ~N[2022-10-19 23:40:00]
+      assert oauth_token.expires_at == ~U[2022-10-19 23:40:00Z]
       assert oauth_token.meta == %{}
       assert oauth_token.name == "some name"
       assert oauth_token.refresh == "some refresh"
@@ -37,12 +37,12 @@ defmodule Sequence.OAuthTokensTest do
 
     test "update_oauth_token/2 with valid data updates the oauth_token" do
       oauth_token = oauth_token_fixture()
-      update_attrs = %{access: "some updated access", deleted_at: ~N[2022-10-20 23:40:00], expires_at: ~N[2022-10-20 23:40:00], meta: %{}, name: "some updated name", refresh: "some updated refresh"}
+      update_attrs = %{access: "some updated access", deleted_at: ~U[2022-10-20 23:40:00Z], expires_at: ~U[2022-10-20 23:40:00Z], meta: %{}, name: "some updated name", refresh: "some updated refresh"}
 
       assert {:ok, %OAuthToken{} = oauth_token} = OAuthTokens.update_oauth_token(oauth_token, update_attrs)
       assert oauth_token.access == "some updated access"
-      assert oauth_token.deleted_at == ~N[2022-10-20 23:40:00]
-      assert oauth_token.expires_at == ~N[2022-10-20 23:40:00]
+      assert oauth_token.deleted_at == ~U[2022-10-20 23:40:00Z]
+      assert oauth_token.expires_at == ~U[2022-10-20 23:40:00Z]
       assert oauth_token.meta == %{}
       assert oauth_token.name == "some updated name"
       assert oauth_token.refresh == "some updated refresh"
