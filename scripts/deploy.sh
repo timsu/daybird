10eacd29f2673dd37e48618d08d4e0e40ef0ed8c
Tim Su <tim@daybird.app>
2022-10-27 22:40:29 -0700

    Blue green deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+cd assets && yarn tsc && cd ..
+git push -f origin HEAD:production
+
+LIVE_VERSION=$(curl -s -w "\n" "https://daybird.app/deployment_id")
+
+if [ "$LIVE_VERSION" = "blue" ]; then
+  DEPLOY_VERSION="green"
+else
+  DEPLOY_VERSION="blue"
+fi
+
+echo "Deploying "$DEPLOY_VERSION
+
 set -e
 
-cd assets
-yarn tsc
-cd ..
+DIR=sites/$DEPLOY_VERSION
 
-git push origin HEAD:production
-
-ssh app@app.listnote.co 'cd listnote; git fetch && git reset --hard origin/production && ./scripts/build.sh && sudo systemctl restart listnote'
+# build & start new server
+ssh app@app.listnote.co "cd $DIR; git fetch && git reset --hard origin/production && \
+  ./scripts/build.sh && \
+  sudo systemctl restart daybird-$DEPLOY_VERSION && \
+  sudo ln -sf /etc/nginx/sites-available/$DEPLOY_VERSION /etc/nginx/sites-enabled/daybird && \
+  sudo systemctl reload nginx"
